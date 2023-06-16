f9399474a4897c87a87e0327376f2bf2fe5ed520
Kutejiang <3076584713@qq.com>
2023-06-16 22:12:57 +0800

    Add files via upload

---
+++ Workflow-Core工作流的控制台使用/EDC.WorkflowCore.Sample/EDC.WorkflowCore.Sample.ConsolePortal/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using WorkflowCore.Interface;
+using EDC.WorkflowCore.Sample.ConsolePortal.Workflows;
+
+namespace EDC.WorkflowCore.Sample.ConsolePortal
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var serviceProvider = ConfigureServices();
+            var host = serviceProvider.GetService<IWorkflowHost>();
+            host.RegisterWorkflow<HelloWorldWorkflow>();
+            host.RegisterWorkflow<OutcomeWorkflow, MyData>();
+            host.RegisterWorkflow<PassingDataWorkflow, MyDataClass>();
+            host.Start();
+
+            // Demo1: Hello World
+            //host.StartWorkflow("HelloWorld");
+
+            // Demo2:Multiple Outcome
+            //Console.WriteLine("Starting workflow...");
+            //string workflowId = host.StartWorkflow("outcompe-sample").Result;
+
+            // Demo3: Passing data between steps
+            Console.WriteLine("Starting workflow...");
+            string workflowId2 = host.StartWorkflow("PassingDataWorkflow").Result;
+
+            Console.ReadKey();
+            host.Stop();
+        }
+
+        private static IServiceProvider ConfigureServices()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddLogging(); // WorkflowCore需要用到logging service
+            services.AddWorkflow(x => x.UseSqlServer(@"server=.;database=WorkflowCoreSQL;uid=sa;pwd=123"
+            , true, true));
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            //var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+            //loggerFactory.AddDebug();
+            return serviceProvider;
+        }
+    }
+}
